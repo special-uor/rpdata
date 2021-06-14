becba89cbc017f10b9b047f5cc93f54d4653d7fc
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-06-14 10:26:31 +0100

    Rename main input parameter from .data to .site_tbl

--- R/utils-entity.R
+++ R/utils-entity.R
@@ -1,12 +1,12 @@
 #' Get entities linked to a hydrological basin site
 #'
-#' @param .data \code{tibble} object.
+#' @param .site_tbl \code{tibble} object.
 #' @param ... Optional parameters.
 #'
 #' @rdname get_entities
 #' @export
-get_entities <- function(.data, ...) {
-  UseMethod("get_entities", .data)
+get_entities <- function(.site_tbl, ...) {
+  UseMethod("get_entities", .site_tbl)
 }
 
 #' @rdname get_entities
@@ -17,25 +17,25 @@ get_entities <- function(.data, ...) {
 #' conn <- open_conn_mysql("sys", "root")
 #' get_entities(conn)
 #' }
-get_entities.site <- function(.data, ...) {
+get_entities.site <- function(.site_tbl, ...) {
   aux <- tibble::tibble()
-  if ("ID_SITE" %in% colnames(.data))
+  if ("ID_SITE" %in% colnames(.site_tbl))
     aux <- rpdata::entity %>%
-      dplyr::filter(ID_SITE %in% .data$ID_SITE)
+      dplyr::filter(ID_SITE %in% .site_tbl$ID_SITE)
   if (ncol(aux) == 0)
     aux <- rpdata::entity %>%
-      dplyr::filter(site_name %in% .data$site_name)
+      dplyr::filter(site_name %in% .site_tbl$site_name)
   aux
 }
 
 #' @rdname get_entities
 #' @export
-get_entities.default <- function(.data, ...) {
+get_entities.default <- function(.site_tbl, ...) {
   warning("That doesn't look like a hydrological basin site.")
 }
 
 #' @rdname get_entities
 #' @export
-get_entities.tbl_df <- function(.data, ...) {
+get_entities.tbl_df <- function(.site_tbl, ...) {
   warning("That doesn't look like a hydrological basin site.", call. = FALSE)
 }
--- R/utils-publication.R
+++ R/utils-publication.R
@@ -1,12 +1,12 @@
 #' Get publications linked to a hydrological basin site
 #'
-#' @param .data \code{tibble} object.
+#' @param .site_tbl \code{tibble} object.
 #' @param ... Optional parameters.
 #'
 #' @rdname get_publications
 #' @export
-get_publications <- function(.data, ...) {
-  UseMethod("get_publications", .data)
+get_publications <- function(.site_tbl, ...) {
+  UseMethod("get_publications", .site_tbl)
 }
 
 #' @rdname get_publications
@@ -17,19 +17,19 @@ get_publications <- function(.data, ...) {
 #' conn <- open_conn_mysql("sys", "root")
 #' get_publications(conn)
 #' }
-get_publications.site <- function(.data, ...) {
+get_publications.site <- function(.site_tbl, ...) {
   aux <- tibble::tibble()
-  if ("ID_SITE" %in% colnames(.data))
+  if ("ID_SITE" %in% colnames(.site_tbl))
     aux <- rpdata::entity %>%
-      dplyr::filter(ID_SITE %in% .data$ID_SITE)
+      dplyr::filter(ID_SITE %in% .site_tbl$ID_SITE)
   if (ncol(aux) == 0)
     aux <- rpdata::entity %>%
-      dplyr::filter(site_name %in% .data$site_name)
+      dplyr::filter(site_name %in% .site_tbl$site_name)
   aux
 }
 
 #' @rdname get_publications
 #' @export
-get_publications.default <- function(.data, ...) {
+get_publications.default <- function(.site_tbl, ...) {
   warning("That doesn't look like a hydrological basin site.")
 }
