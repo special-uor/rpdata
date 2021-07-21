c11e0dc4d9a94da70ce3390f87bce064cc82558a
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-07-21 19:26:45 +0100

    Update the get_publication.site generic method

--- R/utils-publication.R
+++ R/utils-publication.R
@@ -21,10 +21,14 @@ get_publications.site <- function(.site_tbl, ...) {
   aux <- tibble::tibble()
   if ("ID_SITE" %in% colnames(.site_tbl))
     aux <- rpdata::entity %>%
-      dplyr::filter(ID_SITE %in% .site_tbl$ID_SITE)
+      dplyr::filter(ID_SITE %in% .site_tbl$ID_SITE) %>%
+      dplyr::left_join(rpdata::entity_link_publication, by = "ID_ENTITY") %>%
+      dplyr::left_join(rpdata::publication, by = "ID_PUB")
   if (ncol(aux) == 0)
     aux <- rpdata::entity %>%
-      dplyr::filter(site_name %in% .site_tbl$site_name)
+      dplyr::filter(site_name %in% .site_tbl$site_name) %>%
+      dplyr::left_join(rpdata::entity_link_publication, by = "ID_ENTITY") %>%
+      dplyr::left_join(rpdata::publication, by = "ID_PUB")
   aux
 }
 
