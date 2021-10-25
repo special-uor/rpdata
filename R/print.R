e5dd33a865a6c1ba9f7e32342f0274e272901644
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-10-25 09:48:21 +0100

    Add new generic methods for print

--- R/print.R
+++ R/print.R
@@ -1,4 +1,35 @@
 #' @export
-print.site <- function(x, ..., n = NULL, width = NULL, n_extra = NULL) {
+print.age_model <- function(x, width = NULL, ..., n = NULL,
+                            max_extra_cols = NULL, max_footer_lines = NULL) {
+  NextMethod()
+}
+
+#' @export
+print.chronology <- function(x, width = NULL, ..., n = NULL,
+                             max_extra_cols = NULL, max_footer_lines = NULL) {
+  NextMethod()
+}
+
+#' @export
+print.date_info <- function(x, width = NULL, ..., n = NULL,
+                            max_extra_cols = NULL, max_footer_lines = NULL) {
+  NextMethod()
+}
+
+#' @export
+print.entity <- function(x, width = NULL, ..., n = NULL, max_extra_cols = NULL,
+                         max_footer_lines = NULL) {
+  NextMethod()
+}
+
+#' @export
+print.sample <- function(x, width = NULL, ..., n = NULL, max_extra_cols = NULL,
+                         max_footer_lines = NULL) {
+  NextMethod()
+}
+
+#' @export
+print.site <- function(x, width = NULL, ..., n = NULL, max_extra_cols = NULL,
+                       max_footer_lines = NULL) {
   NextMethod()
 }
